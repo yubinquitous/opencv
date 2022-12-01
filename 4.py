3d774758c469b182e740af73bad7903d62369ce8
yubinquitous <yb9854@naver.com>
2022-12-01 22:14:23 +0900

    [FEAT] hw4 only img3_3, img3_4

--- 4.py
+++ 4.py
@@ -1,44 +1,71 @@
 import math as math
+
 import cv2 as cv2
 
 
-def main():
-    src = cv2.imread('images/case4/img4_1.png', cv2.IMREAD_COLOR)
+def setLabel(img, pts, label):
+    (x, y, w, h) = cv2.boundingRect(pts)
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    cv2.rectangle(img, pt1, pt2, (255, 255, 0), 1)
+    cv2.putText(img, label, pt1, cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255))
+
+
+def find_eyes(i):
+    file_name = 'images/case4/img4_' + str(i) + '.png'
+    src = cv2.imread(file_name, cv2.IMREAD_COLOR)
 
     if src is None:
         print('Image load failed!')
         return
 
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    blur = cv2.blur(gray, (3, 3))
-    _, img_bin = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+    gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 3)  # 적응형 이진화
+    # # gausian blur
+    gray = cv2.GaussianBlur(gray, (1, 1), 3)
+    # unsharp mask filter
+    gray = cv2.GaussianBlur(gray, (0, 0), 1)
+    alpha = 1.0
+    gray = cv2.addWeighted(gray, 1 + alpha, gray, -alpha, 0)
+    # # bilateralFilter
+    gray = cv2.bilateralFilter(gray, -1, 10, 15)
+    # morphologyEx
+    # gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, None)
+    # gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, None)
+
+    img_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 4)  # 적응형 이진화
     contours, _ = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    count = -1
-    circles = []
     for pts in contours:
-        if cv2.contourArea(pts) < 40:
+        if cv2.contourArea(pts) < 200:
             continue
 
-        approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True) * 0.02, True)
+        approx = cv2.approxPolyDP(pts, cv2.arcLength(pts, True) * 0.2, True)
 
         vtc = len(approx)
 
         if vtc == 4:
-            count += 1
-            circles.append(0)
+            setLabel(src, pts, 'RECT')
         else:
             length = cv2.arcLength(pts, True)
             area = cv2.contourArea(pts)
             ratio = 4. * math.pi * area / (length * length)
 
-            if ratio > 0.8:
-                circles[count] += 1
+            if ratio > 0.85:
+                setLabel(src, pts, 'CIR')
 
-    circles.sort()
-    for i in circles:
-        if i != 0:
-            print(i)
+    cv2.imshow('src' + str(i), src)
+    cv2.imshow('img_bin' + str(i), img_bin)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+def main():
+    # for i in range(1, 8):
+    #     find_eyes(i)
+
+    find_eyes(7)
 
 
 main()
