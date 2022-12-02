74cf9b833586b4137c70dab336be2b0e044c152c
yubinquitous <yb9854@naver.com>
2022-12-02 19:36:35 +0900

    [FEAT] hw3 1, 3

--- 3.py
+++ 3.py
@@ -1,12 +1,15 @@
 import math as math
+
 import cv2 as cv2
 
-# def set_label(img, pts, label):
-#     (x, y, w, h) = cv2.boundingRect(pts)
-#     pt1 = (x, y)
-#     pt2 = (x + w, y + h)
-#     cv2.rectangle(img, pt1, pt2, (255, 255, 0), 1)
-#     cv2.putText(img, label, pt1, cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255))
+
+def set_label(img, pts, label):
+    (x, y, w, h) = cv2.boundingRect(pts)
+    pt1 = (x, y)
+    pt2 = (x + w, y + h)
+    cv2.rectangle(img, pt1, pt2, (255, 255, 0), 1)
+    cv2.putText(img, label, pt1, cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255))
+
 
 def find_eyes(i):
     file_name = 'images/case3/img3_' + str(i) + '.png'
@@ -17,8 +20,10 @@ def find_eyes(i):
         return
 
     gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    blur = cv2.blur(gray, (3, 3))
-    _, img_bin = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    gray = cv2.blur(gray, (3, 3))
+    # adaptive thresholding
+    img_bin = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    img_bin = cv2.blur(img_bin, (3, 3))
     contours, _ = cv2.findContours(img_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     count = -1
@@ -39,24 +44,18 @@ def find_eyes(i):
             area = cv2.contourArea(pts)
             ratio = 4. * math.pi * area / (length * length)
 
-            if ratio > 0.8:
+            if ratio > 0.85:
                 circles[count] += 1
-                # set_label(src, pts, 'CIR') #test
+                set_label(src, pts, 'CIR')  # test
 
     circles.sort()
     for j in circles:
         if j != 0:
             print(j)
 
-    cv2.imshow('src' + str(i), src)
-    cv2.imshow('img_bin' + str(i), img_bin)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
 
 def main():
-    for i in range(3, 5):
-        find_eyes(i)
+    find_eyes(2)
 
 
 main()
